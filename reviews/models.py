0af59ce5887d926660bc5554e4cba2084ee722ac
RichBurman <>
2023-11-08 11:25:18 +0000

    Edit review model by adding difficulty rating and overall rating and updating templates to display to user. Basic editing to base.css to see all reviews

--- reviews/models.py
+++ reviews/models.py
@@ -4,11 +4,37 @@ from packages.models import Package
 
 
 class Review(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     package = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True, blank=True)
-    rating = models.PositiveIntegerField(default=5)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    DIFFICULTY_CHOICES = (
+        ('easy', 'Easy'),
+        ('moderate', 'Moderate'),
+        ('difficult', 'Difficult'),
+        ('challenging', 'Challenging'),
+    )
+
+    difficulty_rating = models.CharField(
+        max_length=20,
+        choices=DIFFICULTY_CHOICES,
+        default='easy'
+    )
+
+    OVERALL_RATING_CHOICES = (
+        ('excellent', 'Excellent'),
+        ('good', 'Good'),
+        ('average', 'Average'),
+        ('poor', 'Poor'),
+        ('very_poor', 'Very Poor'),
+    )
+
+    overall_rating = models.CharField(
+        max_length=20,
+        choices=OVERALL_RATING_CHOICES,
+        default='good'
+    )
 
     def __str__(self):
         return f"Review by {self.user} at {self.created_at} for {self.package}"
