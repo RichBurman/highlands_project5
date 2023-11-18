ee2335432c34892b3c4bc6e081cb4d4614bfd5a1
RichBurman <rich226402@googlemail.com>
2023-11-18 19:52:15 +0000

    Remove all commented out code

--- user_payment/urls.py
+++ user_payment/urls.py
@@ -1,3 +1,4 @@
+# Not in use
 from django.urls import path
 from . import views
 
@@ -5,5 +6,4 @@ urlpatterns = [
 	path('product_page', views.product_page, name='product_page'),
 	path('payment_successful', views.payment_successful, name='payment_successful'),
 	path('payment_cancelled', views.payment_cancelled, name='payment_cancelled'),
-	# path('stripe_webhook', views.stripe_webhook, name='stripe_webhook'),
 ]
